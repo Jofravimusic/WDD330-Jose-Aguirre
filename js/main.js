e28ce61b77f43f331306d81d80c098e7300c7488
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-06-04 23:49:11 -0300

    week 7 notes and questions added

--- js/main.js
+++ js/main.js
@@ -23,6 +23,10 @@ const links = [
     label: 'To Do List App',
     url: 'week6/index.html',
   },
+  {
+    label: 'Week 7 notes',
+    url: 'week7/index.html',
+  },
 ];
 
 function createList() {
