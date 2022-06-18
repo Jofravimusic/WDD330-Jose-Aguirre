8e9f0cfd241f7049677e4da3d9a087e1ed74ec94
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-06-18 10:06:48 -0300

    Favicon added

--- js/main.js
+++ js/main.js
@@ -27,6 +27,14 @@ const links = [
     label: 'Week 7 notes',
     url: 'week7/index.html',
   },
+  {
+    label: 'Week 8 notes',
+    url: 'week8/index.html',
+  },
+  {
+    label: 'Week 9 notes',
+    url: 'week9/index.html',
+  },
 ];
 
 function createList() {
